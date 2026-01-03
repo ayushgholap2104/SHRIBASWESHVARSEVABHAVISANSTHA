8eb0c98ffff1c505aa7a171db954c8a6aa4cbd50
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-01-03 14:45:46 +0530

    Updated Code

--- js/main.js
+++ js/main.js
@@ -6,8 +6,24 @@ window.addEventListener("load", () => {
   sidebarIcon.addEventListener('click', () => {
     sidebarMenu.classList.toggle('active')
   })
+  mediacont_slider()
 })
 
-function slideNext(){
-  
+function mediacont_slider(){
+  left_slide_btn = document.querySelector('.left_slide_btn')
+  right_slide_btn = document.querySelector('.right_slide_btn')
+  media_container = document.querySelector('.media_displayer')
+  total_media = document.querySelectorAll('.media_cont').length
+  media_index = 0
+
+  right_slide_btn.addEventListener('click',()=>{
+    if (media_index < total_media - 1){
+      media_index++
+    }else{
+      media_index =0
+    }
+    right_slide = media_index * -100
+    media_container.style.transform = `translateX(${right_slide}%)`
+  })
+
 }

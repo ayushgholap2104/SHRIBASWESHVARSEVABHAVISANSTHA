bf5461a770e075b63524196d12f78c4e53957797
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-01-06 16:16:20 +0530

    Updated Code

--- js/main.js
+++ js/main.js
@@ -7,4 +7,61 @@ window.addEventListener("load", () => {
     sidebarMenu.classList.toggle('active')
   })
 })
+const functions = {
+  mediadataBinding: function () {
+    const events = [
+      {
+        id: 1,
+        eventTitle: "Professional beauty parlour course training",
+        eventDesc: `
+                  Under the aegis of Shri Basaveshwar Sevabhavi Sanstha,a
+                  professional beauty parlour training program was organized
+                  with the objective of empowering women through skill
+                  development and entrepreneurship. The program equipped
+                  participants with practical knowledge and hands-on training,
+                  enabling them to build sustainable livelihoods. To encourage
+                  excellence and initiative, the top three participants,
+                  recognized for their outstanding performance and progress,
+                  were awarded a beauty parlour chair and a hair dryer. These
+                  resources were provided to support them in establishing their
+                  own beauty parlour businesses and achieving financial
+                  independence. Such initiatives highlight the organization’s
+                  continued commitment to promoting women’s empowerment,
+                  self-reliance, and inclusive community development
+                `,
+        eventMedia: [
+          "img/Professional_beauty_parlour_course_training.jpg",
+        ]
+      },
+      {
+        id: 2,
+        eventTitle: "Professional beauty parlour course training",
+        eventDesc: `
+                Under the Vavar Mitra / Saheli Training Program, this initiative
+                is being implemented through Shri Basaveshwar Social Welfare
+                Organization with the objective of improving the living
+                standards of farmers in rural areas and promoting sustainable,
+                organic farming practices. As part of the project, trained Vavar
+                Mitra / Saheli members assist farmers in installing a small
+                solar-powered “Jadugar” device. This device helps detect the
+                presence of flies in cattle sheds, fruit flies, and pest density
+                in agricultural fields. By providing accurate monitoring, it
+                reduces the need for excessive chemical spraying and supports
+                healthier crop management while ensuring better yields. Through
+                the promotion of organic farming methods, the project aims to
+                minimize harmful effects on human health, protect the
+                environment, and create long-term benefits for farming
+                communities. This initiative reflects a strong commitment to
+                sustainable agriculture and rural development.
+              `,
+        eventMedia: [
+          "img/Vavar_mitra.jpg",
+          "img/Vavar_mitra1.jpg",
+          "img/Vavar_mitra2.jpg",
+          "img/Vavar_mitra3.jpg",
+        ]
+      },
+    ]
+  }
+}
 

eac1b041ec5e58a2411062496d118ac56c1447e6
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-01-06 18:46:28 +0530

    Updated Code

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ window.addEventListener("load", () => {
   sidebarIcon.addEventListener('click', () => {
     sidebarMenu.classList.toggle('active')
   })
+  functions.mediadataBinding()
 })
 const functions = {
   mediadataBinding: function () {
     const events = [
       {
         id: 1,
-        eventTitle: "Professional beauty parlour course training",
-        eventDesc: `
+        eventTitle: "1) Professional beauty parlour course training",
+        eventDesc:`
                   Under the aegis of Shri Basaveshwar Sevabhavi Sanstha,a
                   professional beauty parlour training program was organized
                   with the objective of empowering women through skill
@@ -27,7 +28,7 @@ const functions = {
                   own beauty parlour businesses and achieving financial
                   independence. Such initiatives highlight the organization’s
                   continued commitment to promoting women’s empowerment,
-                  self-reliance, and inclusive community development
+                  self-reliance, and inclusive community development.
                 `,
         eventMedia: [
           "img/Professional_beauty_parlour_course_training.jpg",
@@ -35,7 +36,7 @@ const functions = {
       },
       {
         id: 2,
-        eventTitle: "Professional beauty parlour course training",
+        eventTitle: "2) Vavar mitra program",
         eventDesc: `
                 Under the Vavar Mitra / Saheli Training Program, this initiative
                 is being implemented through Shri Basaveshwar Social Welfare
@@ -59,9 +60,40 @@ const functions = {
           "img/Vavar_mitra1.jpg",
           "img/Vavar_mitra2.jpg",
           "img/Vavar_mitra3.jpg",
-        ]
+        ],
       },
     ]
+    const project_container = document.querySelector('.project_container')
+    events.forEach(data => {
+      const project = document.createElement('div')
+      project.classList.add('project');
+      const media_id = data.id;
+      const media_title = data.eventTitle;
+      project.setAttribute('project_id', media_id);
+      project.setAttribute('project_title',media_title );
+      project.innerHTML =
+        `
+            <div class="project_title">
+              <h2>${data.eventTitle}</h2>
+            </div>
+            <div class="project_content">
+              <div class="project_content_media">
+                <div class="media_displayer"></div>
+              </div>
+              <div class="project_description">
+                <p>${data.eventDesc}</p>
+              </div>
+            </div>
+      `
+      const media_displayer = project.querySelector('.media_displayer')
+      data.eventMedia.forEach(media_img=>{
+        const media_cont = document.createElement('div')
+        media_cont.classList.add('media_cont')
+        media_cont.innerHTML = `<img src="${media_img}" alt="${data.eventTitle}" srcset="" />`
+        media_displayer.appendChild(media_cont)
+      })
+      project_container.appendChild(project)
+    })
   }
 }
 

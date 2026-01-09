1025a91ff3d848d4694e3e1f5643ef1123b40717
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-01-09 17:19:59 +0530

    Updated Code

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+
+  const sidebarIcon = document.querySelector('.sidebar')
+  const sidebarMenu = document.querySelector('.side_menu')
+
+  sidebarIcon.addEventListener('click', () => {
+    sidebarMenu.classList.toggle('active')
+  })
+})

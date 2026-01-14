9111fc7b0ce417ad07ab3a8ae3569185b26a0a19
ayushgholap2104 <ayushgholap2104@gmail.com>
2026-01-14 13:40:57 +0530

    Updated Code

--- js/contact.js
+++ js/contact.js
@@ -7,3 +7,33 @@ window.addEventListener("load", () => {
     sidebarMenu.classList.toggle('active')
   })
 })
+const form = document.getElementById('contact_form')
+async function feedback_msg(event){
+  event.preventDefault()
+  const status = document.querySelector('.popup')
+  data = new FormData(event.target)
+
+  try {
+    var res = fetch(event.target.action,{
+      method: form.method,
+      body:data,
+      data_type:{'accept':'application/json'}
+    })
+
+    if (res.ok){
+      status.classList.add('active')
+      form.reset()
+    }else{
+      status.innerHTML = 'Oops! There Was a Problem Submitting Your Form'
+    }
+
+  } catch (err) {
+    console.log(err)
+  }
+
+  const close_btn = document.getElementById('close_btn')
+  close_btn.addEventListener('click',()=>{
+    status.classList.remove('active')
+  })
+  form.addEventListener('submit',feedback_msg())
+}
